2abaed090208a5191f4e9b2ed39aeaacf23375e7
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-07-12 18:32:19 -0300

    df final

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def news_teste(df):
                 </style>
                 """
 
-    components.iframe(result_str)
+    components.html(result_str)
     #components.iframe(hide_streamlit_style)
     # st.markdown(result_str, unsafe_allow_html=True)
     # st.markdown(hide_streamlit_style, unsafe_allow_html=True)
