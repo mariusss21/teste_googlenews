5ba34d83fb77a21c3d7cfb571cff9843f40f14f6
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-07-12 18:55:13 -0300

    df final

--- app.py
+++ app.py
@@ -28,7 +28,43 @@ div.block-container{
 
 
 def widget_news():
-    components.html('botton_widget.html')
+    components.html("""
+<html><body>
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://br.tradingview.com/markets/" rel="noopener" target="_blank">
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+  {
+  "symbols": [
+    {
+      "proName": "FOREXCOM:SPXUSD",
+      "title": "S&P 500"
+    },
+    {
+      "proName": "FOREXCOM:NSXUSD",
+      "title": "US 100"
+    },
+    {
+      "proName": "BITSTAMP:BTCUSD",
+      "title": "Bitcoin"
+    },
+    {
+      "proName": "BITSTAMP:ETHUSD",
+      "title": "Ethereum"
+    }
+  ],
+  "showSymbolLogo": true,
+  "colorTheme": "dark",
+  "isTransparent": false,
+  "displayMode": "adaptive",
+  "locale": "br"
+}
+  </script>
+</div>
+<!-- TradingView Widget END -->
+</body></html>
+""")
 
 @st.cache(show_spinner=True, ttl=3600)
 def raw_petro() -> pd.DataFrame:
