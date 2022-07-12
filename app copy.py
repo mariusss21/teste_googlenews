34b13d5b3ffbd7e359153054b571c6e5344f25e8
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-07-12 14:27:02 -0300

    começando

---
+++ app copy.py
@@ -0,0 +1,88 @@
+from datetime import timedelta, date
+import pandas as pd
+from GoogleNews import GoogleNews
+from datetime import datetime
+import streamlit as st
+from time import sleep
+from google.cloud import bigquery
+from google.oauth2 import service_account
+import plotly.graph_objects as go
+
+st.set_page_config(
+    page_title="Análise ações Petrobrás",
+	layout="wide",
+    #initial_sidebar_state="collapsed",
+)
+
+m = st.markdown("""
+<style>
+div.stButton > button:first-child{
+    width: 100%;
+
+}
+</style>""", unsafe_allow_html=True)
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
+
+@st.cache(show_spinner=True, ttl=3600)
+def query_stackoverflow() -> pd.DataFrame:
+    query_job = client.query("SELECT * FROM `marioloc-1491911271221.teste1.tabela1`")
+    df = query_job.result().to_dataframe()  # Waits for job to complete.
+    # df = results.to_dataframe()
+    return df
+    # for row in results:
+    #     st.write(row)
+        
+        
+def stack_minio(request):
+    st.write("Inicio da Function")
+
+    #st.write("Definindo termo para busca da Function")
+    termo = 'petrobras'
+    #st.write("Definindo termo " + termo + " para busca da Function")
+    #st.write("Definindo lang e region")
+    googlenews = GoogleNews(lang='pt', region='BR', period='12h')
+    #st.write("Definindo periodo de 23 horas para busca")
+    #googlenews = GoogleNews(period='12h')
+    #st.write("Fazendo chamada na API do Google News")
+    googlenews.search(termo)
+    #st.write("Termino da chamada na API do Google News")
+    result = googlenews.result().to_dataframe()
+    #st.write("Fazendo o clear na API")
+    googlenews.clear()
+    #st.write("Apresentando resultado")
+    st.write(result)
+    st.write("Termino da Function")
+    
+
+
+
+
+
+if __name__ == '__main__':
+
+    st.subheader('Dados de ações da petrobrás')
+    col1, col2 = st.columns([8, 2])
+
+    st.sidebar.subheader('Stack Labs Finance')
+    buscar = st.sidebar.button('Buscar noticias')
+    select_bq = st.sidebar.button('bigquery test')
+
+    if buscar:
+        stack_minio(None)
+            
+    with col1:
+        if select_bq:
+            df = query_stackoverflow()
+            st.write(df)
+            fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+                        open=df['Open'],
+                        high=df['High'],
+                        low=df['Low'],
+                        close=df['Close'])])
+            st.write(fig)
+
+
+    
--- app.py
+++ app.py
@@ -21,41 +21,34 @@ div.stButton > button:first-child{
 
 }
 </style>""", unsafe_allow_html=True)
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = bigquery.Client(credentials=credentials)
 
 @st.cache(show_spinner=True, ttl=3600)
-def query_stackoverflow() -> pd.DataFrame:
-    query_job = client.query("SELECT * FROM `marioloc-1491911271221.teste1.tabela1`")
-    df = query_job.result().to_dataframe()  # Waits for job to complete.
-    # df = results.to_dataframe()
+def raw_petro() -> pd.DataFrame:
+    df = pd.read_csv('raw_petro.csv')
     return df
-    # for row in results:
-    #     st.write(row)
-        
-        
-def stack_minio(request):
-    st.write("Inicio da Function")
 
-    #st.write("Definindo termo para busca da Function")
-    termo = 'petrobras'
-    #st.write("Definindo termo " + termo + " para busca da Function")
-    #st.write("Definindo lang e region")
-    googlenews = GoogleNews(lang='pt', region='BR', period='12h')
-    #st.write("Definindo periodo de 23 horas para busca")
-    #googlenews = GoogleNews(period='12h')
-    #st.write("Fazendo chamada na API do Google News")
-    googlenews.search(termo)
-    #st.write("Termino da chamada na API do Google News")
-    result = googlenews.result().to_dataframe()
-    #st.write("Fazendo o clear na API")
-    googlenews.clear()
-    #st.write("Apresentando resultado")
-    st.write(result)
-    st.write("Termino da Function")
-    
+
+@st.cache(show_spinner=True, ttl=3600)
+def raw_gnews() -> pd.DataFrame:
+    df = pd.read_csv('raw_gnews.csv')
+    return df
+
+
+@st.cache(show_spinner=True, ttl=3600)
+def final_df() -> pd.DataFrame:
+    df = pd.read_csv('df_final.csv')
+    return df
+
+
+def dashboard(data_inicial, data_final):
+    df_raw_petro = raw_petro()
+    st.write(df_raw_petro)
+
+    df_raw_gnews = raw_gnews()
+    st.write(df_raw_gnews)
+
+    df_final = final_df()
+    st.write(df_final)
 
 
 
@@ -63,26 +56,29 @@ def stack_minio(request):
 
 if __name__ == '__main__':
 
-    st.subheader('Dados de ações da petrobrás')
+    st.title("Stack labs finance ")
+    st.title('Equipe MinIO')
     col1, col2 = st.columns([8, 2])
 
-    st.sidebar.subheader('Stack Labs Finance')
-    buscar = st.sidebar.button('Buscar noticias')
-    select_bq = st.sidebar.button('bigquery test')
+    st.sidebar.subheader('Menu')
+    paginas = ['Análise ações Petrobrás', 'Notícias']
+    pagina = st.sidebar.button('Selecione uma análise', paginas)
+    data_inicial = st.sidebar.date_input('Data inicial', datetime.now() - timedelta(days=1))
+    data_final = st.sidebar.date_input('Data final', datetime.now() - timedelta(days=30))
 
-    if buscar:
-        stack_minio(None)
+    if pagina == 'Análise ações Petrobrás':
+        dashboard(data_inicial, data_final)
             
-    with col1:
-        if select_bq:
-            df = query_stackoverflow()
-            st.write(df)
-            fig = go.Figure(data=[go.Candlestick(x=df['Date'],
-                        open=df['Open'],
-                        high=df['High'],
-                        low=df['Low'],
-                        close=df['Close'])])
-            st.write(fig)
+    # with col1:
+    #     if select_bq:
+    #         df = query_stackoverflow()
+    #         st.write(df)
+    #         fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+    #                     open=df['Open'],
+    #                     high=df['High'],
+    #                     low=df['Low'],
+    #                     close=df['Close'])])
+    #         st.write(fig)
 
 
     
