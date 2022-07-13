067391ccc91faa551146475dd18ffdf74c9192a7
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-07-13 17:33:57 -0300

    dale

--- app.py
+++ app.py
@@ -273,7 +273,7 @@ if __name__ == '__main__':
             news_qtd = st.number_input('Quantidade de notícias', value=10, min_value=1, max_value=1000)
             df_noticias_view = df_raw_gnews_date.sort_values('date', ascending=False)
             df_noticias_view = df_noticias_view.head(int(news_qtd))
-            st.write(df_raw_gnews_date)
+            st.write(df_raw_gnews)
             for i in range(df_noticias_view.shape[0]):
                 row = df_noticias_view.iloc[i]
                 st.markdown("---")
@@ -282,17 +282,5 @@ if __name__ == '__main__':
                     {row['desc']}\n
                     *{row['date']}* 
                     """)
-            
-    # with col1:
-    #     if select_bq:
-    #         df = query_stackoverflow()
-    #         st.write(df)
-    #         fig = go.Figure(data=[go.Candlestick(x=df['Date'],
-    #                     open=df['Open'],
-    #                     high=df['High'],
-    #                     low=df['Low'],
-    #                     close=df['Close'])])
-    #         st.write(fig)
-
 
     
