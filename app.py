0c128450489c78c918caa507eb803bfe93ce9087
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-07-14 22:38:11 -0300

    novo ensemble

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ def model_chart(df, df_raw_petro):
 
     #df_chart['colors'] = 'green' if df_chart['y_pred'] == 1 else 'red'
     df_chart['colors'] = ['green' if df_chart['y_pred'] == 1 else 'red']
-    #list_colors = list(df_chart)
+    list_colors = list(df_chart)
 
     fig = go.Figure(data=[go.Bar(x=df_raw_petro.Date,
                         y=df_chart.y_pred,
