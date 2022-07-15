17fe441f7fa39310f55f1951bbff1ae2d95f99b3
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-07-14 22:36:05 -0300

    novo ensemble

--- app.py
+++ app.py
@@ -170,7 +170,7 @@ def predict_model(df):
 
 def model_chart(df, df_raw_petro):
     df_aux = df.copy()
-    df_aux['Date'] = pd.to_datetime(df_aux['Date'])
+    df_aux['Date'] = pd.to_datetime(df_aux['Date']).dt.date
     df_chart = df_raw_petro.merge(df_aux[['Date', 'y_pred']], on='Date', how='left')
     df_chart.loc[df_chart['y_pred'] == 0] = -1
     df_chart['y_pred'].fillna(0, inplace=True)
