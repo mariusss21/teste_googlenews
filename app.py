e04e8935cd221cee14cde6b17118ab2931058d35
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-07-18 14:44:48 -0300

    atualizando dataframe consolidado

--- app.py
+++ app.py
@@ -309,8 +309,7 @@ def dashboard(data_inicial, data_final):
     df_ibov.rename(columns={'Datetime': 'Date'}, inplace=True)
 
     df_predict = predict_model(df_final)
-    previsao = df_predict.loc[(df_predict['Date'].dt.date == date.today(), 'y_pred')]
-    st.write(previsao)
+    previsao = df_predict.loc[(df_predict['Date'].dt.date == date.today(), 'y_pred')].item()
 
     with col1:
         if tipo_cotacao == 'Histórica':
